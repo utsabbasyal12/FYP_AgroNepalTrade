9c6d1578437c06e6af6c793dc29ba9ba5891caeb
Username <utsabbasyal12@gmail.com>
2022-03-31 23:09:28 +0545

    Added Blog Manager

---
+++ BlogManagers/BlogBusinessManager.cs
@@ -0,0 +1,36 @@
+﻿using AgroNepalTrade.Models;
+using FYP_AgroNepalTrade.BlogManagers.Interfaces;
+using FYP_AgroNepalTrade.Models.BlogViewModels;
+using FYP_AgroNepalTrade.Models.Services;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FYP_AgroNepalTrade.BlogManagers
+{
+    public class BlogBusinessManager : IBlogBusinessManager
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IBlogService blogService;
+
+        public BlogBusinessManager(UserManager<ApplicationUser> userManager, IBlogService blogService)
+        {
+            this.userManager = userManager;
+            this.blogService = blogService;
+        }
+        public async Task<Blog> CreateBlog(CreateViewModel createViewModel, ClaimsPrincipal claimsPrincipal)
+        {
+            Blog blog = createViewModel.Blog;
+
+            blog.Author = await userManager.GetUserAsync(claimsPrincipal);
+            blog.CreatedOn = DateTime.Now;
+            
+            return await blogService.Add(blog);
+
+            
+        }
+    }
+}
---
+++ BlogManagers/Interfaces/IBlogBusinessManager.cs
@@ -0,0 +1,14 @@
+﻿using FYP_AgroNepalTrade.Models.BlogViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FYP_AgroNepalTrade.BlogManagers.Interfaces
+{
+    public interface IBlogBusinessManager
+    {
+        Task<Blog> CreateBlog(CreateViewModel createViewModel, ClaimsPrincipal claimsPrincipal);
+    }
+}
