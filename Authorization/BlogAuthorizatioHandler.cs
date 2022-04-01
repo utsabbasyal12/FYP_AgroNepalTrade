443410fea810e1e5e32f975495174a3cf51a356c
Username <utsabbasyal12@gmail.com>
2022-04-01 21:03:34 +0545

    Blog Authentication

---
+++ Authorization/BlogAuthorizatioHandler.cs
@@ -0,0 +1,31 @@
+﻿using AgroNepalTrade.Models;
+using FYP_AgroNepalTrade.Models.BlogViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP_AgroNepalTrade.Authorization
+{
+    public class BlogAuthorizatioHandler : AuthorizationHandler<OperationAuthorizationRequirement, Blog>
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public BlogAuthorizatioHandler(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, Blog resource)
+        {
+            var applicationUser = await userManager.GetUserAsync(context.User);
+            if(requirement.Name == Operations.Update.Name || requirement.Name == Operations.Delete.Name && applicationUser == resource.Author)
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
