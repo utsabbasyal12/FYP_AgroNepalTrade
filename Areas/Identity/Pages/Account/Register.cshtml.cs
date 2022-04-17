147ec78393b9f02741c6b978b1efe768a092d492
Username <utsabbasyal12@gmail.com>
2022-04-18 02:27:30 +0545

    Added Checkout Form

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
+﻿using AgroNepalTrade.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -14,7 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
-using AgroNepalTrade.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace AgroNepalTrade.Areas.Identity.Pages.Account
 {
@@ -45,32 +44,34 @@ namespace AgroNepalTrade.Areas.Identity.Pages.Account
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-public class InputModel
-{
+        public class InputModel
+        {
+            public string Roles { get; set; }
 
-    public string Roles { get; set; }
-    [Required]
-    [Display(Name = "First Name")]
-    public string FirstName { get; set; }
-    [Required]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; }
-    [Required]
-    [EmailAddress]
-    [Display(Name = "Email")]
-    public string Email { get; set; }
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
 
-    [Required]
-    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-    [DataType(DataType.Password)]
-    [Display(Name = "Password")]
-    public string Password { get; set; }
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
 
-    [DataType(DataType.Password)]
-    [Display(Name = "Confirm password")]
-    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    public string ConfirmPassword { get; set; }
-}
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -82,43 +83,43 @@ public class InputModel
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-if (ModelState.IsValid)
-{
-    MailAddress address = new MailAddress(Input.Email);
-    string userName = address.User; 
-    var user = new ApplicationUser 
-    { 
-        UserName = userName, 
-        Email = Input.Email,
-        FirstName = Input.FirstName,
-        LastName = Input.LastName
-};
-var result = await _userManager.CreateAsync(user, Input.Password);
-if (result.Succeeded)
-{
-    _logger.LogInformation("User created a new account with password.");
-    await _userManager.AddToRoleAsync(user, Input.Roles.ToString());
-    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-    var callbackUrl = Url.Page(
-        "/Account/ConfirmEmail",
-        pageHandler: null,
-        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-        protocol: Request.Scheme);
+            if (ModelState.IsValid)
+            {
+                MailAddress address = new MailAddress(Input.Email);
+                string userName = address.User;
+                var user = new ApplicationUser
+                {
+                    UserName = userName,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, Input.Roles.ToString());
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
 
-    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-    {
-        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-    }
-    else
-    {
-        await _signInManager.SignInAsync(user, isPersistent: false);
-        return LocalRedirect(returnUrl);
-    }
-}
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using AgroNepalTrade.Enums;
+using FYP_AgroNepalTrade.ProductManagers;
+using FYP_AgroNepalTrade.ProductManagers.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP_AgroNepalTrade.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IProductBusinessManager productBusinessManager;
+
+        public CustomerController(IWebHostEnvironment webHostEnvironment, IProductBusinessManager productBusinessManager)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.productBusinessManager = productBusinessManager;
+        }
+        [HttpGet]
+        [Authorize(Roles = "Customer")]
+        public IActionResult SendRequest()
+        {
+            return View();
+        }
+       /* public async Task<IActionResult> SendRequest(int? id)
+        {
+            var actionResult = await productBusinessManager.GetProductViewModel(id, User);
+
+            if (actionResult.Result is null)
+                return View(actionResult.Value);
+
+            return actionResult.Result;
+        }*/
+    }
+}
